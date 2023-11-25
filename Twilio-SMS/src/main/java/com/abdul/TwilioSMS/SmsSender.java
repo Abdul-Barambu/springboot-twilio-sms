ffb0b15c2232a3a2637c3c9d624887d19369521b
Abdul Barambu <abdulkadirbarambu1@gmail.com>
2023-11-25 21:02:35 +0100

    first commit

---
+++ Twilio-SMS/src/main/java/com/abdul/TwilioSMS/SmsSender.java
@@ -0,0 +1,5 @@
+package com.abdul.TwilioSMS;
+
+public interface SmsSender {
+    void sendSms(SmsRequest smsRequest);
+}
---
+++ Twilio-SMS/src/test/java/com/abdul/TwilioSMS/TwilioSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdul.TwilioSMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilioSmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
